3d1ba0f0b7c442c652b0987dee15c122efb4c9fe
FRC ROBOTICS PAGE <53526469+deastrobooking@users.noreply.github.com>
2022-04-10 00:17:50 -0400

    Update 8

--- itwp1100/8a/script.js
+++ itwp1100/8a/script.js
@@ -1,24 +1,24 @@
 /*
-*      JavaScript 6th Edition
-*      Chapter 8
-*      Hands-on Project 8-3
-
-*      Author: Randolph Chabot
-*      Date:   4/4/22
-
-*      Filename: script.js
+      JavaScript 6th Edition
+      Chapter 8
+      Hands-on Project 8-3
+      Author: Randolph Chabot
+      Date:   4/7/22
+      Filename: script.js
 */
 
 "strict mode";
 
 function selectCardType() {
-    
+    //Declaring Regex Conditions. 
+    //https://stackoverflow.com/questions/14850553/javascript-regex-for-password-containing-at-least-8-characters-1-number-1-uppe
+    //----------------------------------------------------------------------------
     var cardNumValue = document.getElementById("ccNum").value
     var visa = /^4[0-9]{12}(?:[0-9]{3})?$/
     var mc= /^5 [1-5][0-9]{14}$/
     var discover = /^6(?:011| 5[0-9]{2}) [0-9]{12}$/
     var amex = /^3[47][0-9]{13}$/
-    
+    // Conditional Statement Testing Card Values. ________________________________
     if (visa.test(cardNumValue)) { 
         document.getElementById("visa").checked = "checked";
     } else if (mc.test(cardNumValue)) {
@@ -28,8 +28,10 @@ function selectCardType() {
     } else if (discover.test(cardNumValue)) {
         document.getElementById("discover").checked= "checked";
     }
+//________________________________________________________________________________
+//Creating an Event Listener........
+//________________________________________________________________________________
 }
-
 function createEventListeners() {
     var cardNum = document.getElementById("ccNum");
     if (cardNum.addEventListener) {
@@ -38,7 +40,7 @@ function createEventListeners() {
         cardNum.attachEvent("onchange",selectCardType);
     }
 }
-
+//event listener continued....___________________________________________________
 if(window.addEventListener) {
     window.addEventListener("load", createEventListeners, false);
 }else if (window.attachEvent) {
--- itwp1100/8b/script.js
+++ itwp1100/8b/script.js
@@ -2,14 +2,18 @@
       JavaScript 6th Edition
       Chapter 8
       Hands-on Project 8-4
-
       Author: Randolph Chabot
-      Date:   4/4/22
-
+      Date:   4/7/22
       Filename: index.html
 */
-
+//__--__--__ Declaring a variable in scrict mode. Variable Declared as an empty array. 
+'use strict';
 var list  = [];
+//___________________________________________________________________________________
+//Function Loops Through <li> elements removes them. 
+// Then Loops throught the list array and fro each array element it creates a new li element.
+// It then generates the content of that element using the current array element alo0ng with a 
+// span element to create the "first" button. Then creates event listener. 
 
 function generateList() {
     var listItems = document.getElementsByTagName("li");
@@ -33,6 +37,9 @@ function generateList() {
         }
     }
 }
+// __________________________________________________________________________________
+// uses push() method to adds the value in the text box to the end of the list array. 
+// returns focus to text box, clears its value, then calls generateList()
 
 function addItem() {
     var newItem = document.getElementById("newItem");
@@ -41,9 +48,15 @@ function addItem() {
     newItem.value = "";
     generateList();
 }
+// 1.func takes parameter (evt) creates variables that reference collection of <li> elements 
+// and the parent <li> element of the "first" button. 2. Then checks using for and search() method
+// to check each value in the "list" array against content of parent <li> element 
+// 3. Uses splice() method to remove array el. from the array and assign it to a variable. 
+// 4. Then uses unshift() to add var as a new el. to start of []  
+// 5. Then calls generateList() reacreate list based on order.
 
 function moveToTop(evt) {
-    if (evt === undefined) { // get caller element in IE8
+    if (evt === undefined) { // uses ===  strict comparison operator. 
         evt = window.event;
     }
     var callerElement = evt.target || evt.srcElement;
@@ -57,6 +70,8 @@ function moveToTop(evt) {
     }
     generateList();
 }
+
+// Creates Event Listener. 
 function createEventListener() {
     var addButton = document.getElementById("button");
     if (addButton.addEventListener) {
