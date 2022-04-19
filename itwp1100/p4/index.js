d5e5ebc35ab786c6ab06a479a659c47a040116db
FRC ROBOTICS PAGE <53526469+deastrobooking@users.noreply.github.com>
2022-04-18 21:47:47 -0400

    Password Update

---
+++ itwp1100/p4/index.js
@@ -0,0 +1,13 @@
+var password = "12345"
+
+function passcheck() {
+    if(document.getElementById('pass1').value =! password){
+        alert('Wrong Password Try Again')
+        return false;
+    }
+
+    if(document.getElementById('pass1').value == password) {
+        alert('Correct Password. Click Ok to enter Webpage')
+        return true;
+    }
+}
