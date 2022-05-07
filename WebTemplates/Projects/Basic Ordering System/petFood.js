b446231cca01cc156e23a49ee6b1c1fd1d603a02
FRC ROBOTICS PAGE <53526469+deastrobooking@users.noreply.github.com>
2022-05-06 21:22:52 -0400

    examples

---
+++ WebTemplates/Projects/Basic Ordering System/petFood.js
@@ -0,0 +1,9 @@
+document.write("<br><br>Today 68% of Americans own dogs and cats. Most owners do not check the ingredients in their pet’s food. ");
+document.write("Pet owners depend on pet food companies and veterinarians to be knowledgeable about the nutrition our pets need. <br><br>");
+document.write("We know what food is healthy for our bodies and our pets depend on us to know what is healthy for them. ");
+document.write("Pet owners should be aware of the ingredients in their pet’s food and the benefits of feeding them healthier food. <br><br>");
+document.write("There are many resources available on nutrition for cats and dogs. ");
+document.write("Pets.webmd.com is a good resource. ");
+document.write("We know what is healthy for our bodies, so we should know what food is healthy for our pets. ");
+document.write("If you consider your pet as part of your family, please research and check the ingredients on ");
+document.write("your pet’s food bag and check online for petfood recalls.");
