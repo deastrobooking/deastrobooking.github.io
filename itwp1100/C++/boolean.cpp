547f321d08b25cd4babcca0e4d95c1437c5e21ef
Tesa FV <53526469+deastrobooking@users.noreply.github.com>
2022-07-21 11:14:20 -0400

    a

---
+++ itwp1100/C++/boolean.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <iostream>
+using std::cout;
+
+int main()
+{
+    bool pizza_is_good = 0;
+    cout << pizza_is_good << 
+}
+
+/*
+{
+    bool a = true; 
+    bool b = false;
+    if(a && b) {
+        puts("true");
+    } esle {
+        puts("false");
+
+    }
+    }
+    printf("a is %d\n", a);
+    printf("b is %d\n", b);
+
+    return 0;
+}
+
+*/
+
+
+ 
